d182dc00691164110dfe345917e35c6eb9fc0fd5
Aditya Borse <aditya2352k@gmail.com>
2025-11-18 21:45:02 -0500

    Add some git opts
    eg. line numers, relative line numers, no bg for line and sign column, tabspaces, smartindent, etc

--- init.lua
+++ init.lua
@@ -1,2 +1,21 @@
 require("config.lazy")
 require("odin")
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.smartindent = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.cmd("hi LineNr guibg=NONE")
+vim.cmd("hi SignColumn guibg=NONE")
+vim.opt.isfname:append("@-@")
+
